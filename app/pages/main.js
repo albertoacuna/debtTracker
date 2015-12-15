c3845320bd7ac2962249aaa8a0928d57455ecc36
albertoacuna <aacuna3@asu.edu>
2015-12-14 19:52:13 -0700

    Got main controller working

--- app/pages/main.js
+++ app/pages/main.js
@@ -1,3 +1,23 @@
 /**
  * Created by dascruel on 12/14/2015.
  */
+(function(){
+    angular.module('app')
+        .controller('mainController', [mainController])
+        .config(config);
+
+    function config($routeProvider){
+      $routeProvider.when('/main',
+          {
+             templateUrl: 'pages/main.html',
+              controller: 'mainController',
+              controllerAs: 'vm'
+          });
+    }
+
+    function mainController(){
+        var vm = this;
+
+        vm.test = 'Test';
+    }
+})();
