d76a31158f414d4959cf62e85845f0bb09be50fb
das_cruel <Sahm6969>
2015-12-16 17:32:27 -0700

    3rd commit

--- app/pages/main.js
+++ app/pages/main.js
@@ -1,23 +1,33 @@
 /**
  * Created by dascruel on 12/14/2015.
  */
-(function(){
-    angular.module('app')
-        .controller('mainController', [mainController])
-        .config(config);
+(function() {
+        angular.module('app')
+            .controller('mainController', ['$http', mainController])
+            .config(config);
 
-    function config($routeProvider){
-      $routeProvider.when('/main',
-          {
-             templateUrl: 'pages/main.html',
-              controller: 'mainController',
-              controllerAs: 'vm'
-          });
+        function config($routeProvider) {
+            $routeProvider.when('/main',
+                {
+                    templateUrl: 'pages/main.html',
+                    controller: 'mainController',
+                    controllerAs: 'vm'
+                });
+        }
+
+        function mainController($http) {
+            var vm = this;
+
+           function getAccounts() {
+                var url = "http://localhost:3000/getMintInfo?";
+                $http.get(url).then(function(response){
+                    vm.accountInfo = response;
+                });
+            };
+
+            getAccounts();
+        }
+
+
     }
-
-    function mainController(){
-        var vm = this;
-
-        vm.test = 'Test';
-    }
-})();
+)();
